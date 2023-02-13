b2677abbcf0387531d216003e0e5485bc98b5301
Jacob Gil <jacobgil325@gmail.com>
2023-02-12 20:03:40 -0500

    styled and completed login component

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -39,28 +39,49 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
+    <form className="rounded-lg bg-white p-6 shadow-md" onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label
+          className="mb-2 block font-medium text-gray-700"
+          htmlFor="username"
+        >
+          Username:
+        </label>
         <input
+          className="w-full rounded-lg border border-gray-400 p-2"
           type="text"
           id="username"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
         />
-        {usernameError && <div>{usernameError}</div>}
+        {usernameError && (
+          <div className="mt-2 text-xs text-red-500">{usernameError}</div>
+        )}
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
+      <div className="mb-4">
+        <label
+          className="mb-2 block font-medium text-gray-700"
+          htmlFor="password"
+        >
+          Password:
+        </label>
         <input
+          className="w-full rounded-lg border border-gray-400 p-2"
           type="password"
           id="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
-        {passwordError && <div>{passwordError}</div>}
+        {passwordError && (
+          <div className="mt-2 text-xs text-red-500">{passwordError}</div>
+        )}
       </div>
-      <button type="submit">Login</button>
+      <button
+        className="w-full rounded-lg bg-indigo-500 py-2 px-4 font-medium text-white hover:bg-indigo-700"
+        type="submit"
+      >
+        Login
+      </button>
     </form>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,8 +3,10 @@ import LoginForm from "../components/forms/LoginForm";
 
 const login = () => {
   return (
-    <div>
-      login
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="mx-10 mb-4 rounded-lg bg-white p-6 text-center text-3xl font-medium shadow-md">
+        Login
+      </h1>
       <LoginForm></LoginForm>
     </div>
   );

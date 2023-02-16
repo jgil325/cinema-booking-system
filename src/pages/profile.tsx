be05864b2399dbe5049ca8216ce38776481a9fca
matthew-ely <mwe70005@uga.edu>
2023-02-15 22:01:34 -0500

    added edit profile forms and styling for edit prof

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import EditProfileForm from '../components/forms/EditProfileForm';
 
-const profile = () => {
+const Profile = () => {
+
+  const handleSubmit = (username: string, phoneNumber: string, password: string) => {
+    console.log(`Submitting login credentials:`);
+    console.log(`Username: ${username}`);
+    console.log(`Username: ${phoneNumber}`);
+    console.log(`Password: ${password}`);
+    // Perform actual login logic here
+  };
+
   return (
-    <div>profile</div>
-  )
-}
+    <div>
+      <div className='flex flex-col items-center mt-6 gap-5'>
+        <div className='px-48 py-8 bg-indigo-500 rounded-3xl shadow-lg flex flex-row'>
+          <h1 className='font-semibold text-2xl text-white'>Current User</h1>
+          
+        </div>
+        <EditProfileForm onSubmit={handleSubmit}/>
+      </div>
+    </div>
+  );
+};
 
-export default profile
+export default Profile;

92dee41da771cf96ee710aaa86b7f05688f3dee9
Jacob Gil <jacobgil325@gmail.com>
2023-02-19 22:34:26 -0500

    registration confirmation page link to login done

--- src/pages/confirmRegistration.tsx
+++ src/pages/confirmRegistration.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const confirmRegistration = () => {
-  return <div>confirmregistration</div>;
+  const router = useRouter();
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="w-96 rounded bg-white p-8 shadow-md">
+        <h2 className="mb-4 text-xl font-bold">Registration Confirmed</h2>
+        <p className="mb-4">
+          Thank you for registering! You should receive a confirmation email
+          shortly.
+        </p>
+        <button
+          onClick={() => router.push("/login")}
+          className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+        >
+          Continue to Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default confirmRegistration;

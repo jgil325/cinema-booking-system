ab6080fb14f796c7fe8167b5a709ab7286e48d69
Jacob Gil <jacobgil325@gmail.com>
2023-04-28 01:20:26 -0400

    book and pay styling

--- src/components/forms/BookAndPay.tsx
+++ src/components/forms/BookAndPay.tsx
@@ -6,20 +6,24 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
     <div>
       <h1 className="text-center font-bold">Book And Pay</h1>
 
-      <div className="grid grid-cols-3 text-left">
-        <span className="pr-2">Seat Number</span>
-        <span className="pr-2">Ticket Type</span>
-        <span className="pr-2">Price</span>
-        {tickets.map((ticket) => {
-          return (
-            <>
-              <span>{ticket.seatNumber}</span>
-              <span>{ticket.type}</span>
-              <span>${ticket.price}</span>
-            </>
-          );
-        })}
-      </div>
+      <table className="w-full table-fixed">
+        <thead>
+          <tr className="bg-gray-200 text-gray-700">
+            <th className="w-1/3 py-2">Seat Number</th>
+            <th className="w-1/3 py-2">Ticket Type</th>
+            <th className="w-1/3 py-2">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket) => (
+            <tr key={ticket.id}>
+              <td className="py-2 text-center">{ticket.seatNumber}</td>
+              <td className="py-2 text-center">{ticket.type}</td>
+              <td className="py-2 text-center">${ticket.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };

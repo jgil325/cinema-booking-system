bd21de612e71c342591f6be6f4be74774fbc926c
Jacob Gil <jacobgil325@gmail.com>
2023-04-28 01:33:46 -0400

    styling

--- src/components/forms/BookAndPay.tsx
+++ src/components/forms/BookAndPay.tsx
@@ -1,7 +1,26 @@
 import type { Ticket } from "@prisma/client";
-import React from "react";
+import React, { useState } from "react";
 
 const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
+  const [paymentCardNumber, setPaymentCardNumber] = useState("");
+  const [promoCode, setPromoCode] = useState("");
+
+  const handlePaymentCardNumberChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPaymentCardNumber(event.target.value);
+  };
+
+  const handlePromoCodeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPromoCode(event.target.value);
+  };
+
+  const handlePayNowClick = () => {
+    // TODO: handle payment logic here
+  };
+
   return (
     <div>
       <h1 className="text-center font-bold">Book And Pay</h1>
@@ -24,6 +43,39 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
           ))}
         </tbody>
       </table>
+
+      <div className="mt-8">
+        <label htmlFor="paymentCardNumber" className="block font-bold">
+          Payment Card Number
+        </label>
+        <input
+          type="text"
+          id="paymentCardNumber"
+          value={paymentCardNumber}
+          onChange={handlePaymentCardNumberChange}
+          className="w-full rounded-md border border-gray-400 p-2"
+        />
+      </div>
+
+      <div className="mt-8">
+        <label htmlFor="promoCode" className="block font-bold">
+          Promo Code (optional)
+        </label>
+        <input
+          type="text"
+          id="promoCode"
+          value={promoCode}
+          onChange={handlePromoCodeChange}
+          className="w-full rounded-md border border-gray-400 p-2"
+        />
+      </div>
+
+      <button
+        onClick={handlePayNowClick}
+        className="mt-8 rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+      >
+        Pay Now
+      </button>
     </div>
   );
 };

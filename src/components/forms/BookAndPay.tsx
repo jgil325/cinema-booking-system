087756256091c1cacc1accb932dfc14323c65be8
matthew-ely <mwe70005@uga.edu>
2023-04-28 12:29:20 -0400

    api fix and create booking handler set up

--- src/components/forms/BookAndPay.tsx
+++ src/components/forms/BookAndPay.tsx
@@ -3,15 +3,36 @@ import React, { useState } from "react";
 import { api } from "../../utils/api";
 
 const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
+  const getBookingFee = api.fees.getAllFees.useQuery();
+  var price = getBookingFee.data?.bookingFee;
+  const tax = 1.1;
+  var total = price * tax;
   const [paymentCardNumber, setPaymentCardNumber] = useState("");
+  const [paymentCardMonth, setPaymentCardMonth] = useState("");
+  const [paymentCardYear, setPaymentCardYear] = useState("");
   const [promoCode, setPromoCode] = useState("");
+  const [totalPrice, setTotalPrice] = useState(total)
 
   const { data: cards } = api.paymentCard.byId.useQuery();
+  const getPromo = api.promos.byCode.useMutation();
+  const createBooking = api.booking.bookAndPay.useMutation();
+  
+  console.log(cards)
 
   const handlePaymentCardNumberChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setPaymentCardNumber(event.target.value);
+  };
+  const handlePaymentCardMonthChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPaymentCardMonth(event.target.value);
+  };
+  const handlePaymentCardYearChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPaymentCardYear(event.target.value);
   };
 
   const handlePromoCodeChange = (
@@ -20,8 +41,33 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
     setPromoCode(event.target.value);
   };
 
-  const handlePayNowClick = () => {
-    // TODO: handle payment logic here
+  const handlePromoCode = async() => {
+    console.log(promoCode)
+    try {
+      const foundPromo = await getPromo.mutateAsync({ code: promoCode});
+      console.log(foundPromo)
+      var price = totalPrice;
+      setTotalPrice(price * foundPromo.discount)
+    } catch {
+      alert(`No promo was found by code: ${promoCode}`);
+    }
+  }
+
+  const setCardDetails = (card) => {
+    setPaymentCardNumber(card.cardNumber)
+    setPaymentCardMonth(card.expirationMonth)
+    setPaymentCardYear(card.expirationYear)
+  }
+
+  const handlePayNowClick = async() => {
+    try {
+      const newBooking = await createBooking.mutateAsync({tickets: tickets, paymentCardNumber: paymentCardNumber})
+      console.log(newBooking)
+      alert('Successfully created booking!')
+    } catch {
+      alert('Error creating your booking.')
+    }
+    
   };
 
   return (
@@ -58,6 +104,7 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
                 <button
                   className="rounded border bg-gray-300 px-1"
                   key={card.id}
+                  onClick={() => {setCardDetails(card)}}
                 >
                   Card Ending In {card.cardNumber.slice(-4)}
                 </button>
@@ -79,6 +126,30 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
           className="w-full rounded-md border border-gray-400 p-2"
         />
       </div>
+      <div className="mt-4">
+        <label htmlFor="paymentCardNumber" className="block font-bold">
+          Payment Card Month
+        </label>
+        <input
+          type="text"
+          id="paymentCardNumber"
+          value={paymentCardMonth}
+          onChange={handlePaymentCardMonthChange}
+          className="w-full rounded-md border border-gray-400 p-2"
+        />
+      </div>
+      <div className="mt-4">
+        <label htmlFor="paymentCardNumber" className="block font-bold">
+          Payment Card Year
+        </label>
+        <input
+          type="text"
+          id="paymentCardNumber"
+          value={paymentCardYear}
+          onChange={handlePaymentCardYearChange}
+          className="w-full rounded-md border border-gray-400 p-2"
+        />
+      </div>
 
       <div className="mt-8">
         <label htmlFor="promoCode" className="block font-bold">
@@ -92,6 +163,12 @@ const BookAndPay = ({ tickets }: { tickets: Ticket[] }) => {
           className="w-full rounded-md border border-gray-400 p-2"
         />
       </div>
+      <button
+        onClick={handlePromoCode}
+        className="mt-8 rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+      >
+        Enter Promo Code
+      </button>
 
       <button
         onClick={handlePayNowClick}
